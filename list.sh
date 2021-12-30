dbcdde717cf6c9c6d01f92c8c04be1ae6a2ac168
0xBash <76225821+0xBash@users.noreply.github.com>
2021-12-30 23:22:37 +0530

    Update list.sh

--- list.sh
+++ list.sh
@@ -5,14 +5,15 @@ today=$(date +%d-%m-%y)
 echo "choose your number: "
 echo "btw choose wisely :)"
 echo "where E-> /etc, L-> /lib, R-> /root, B-> /bin, H-> /home: "
-if [ $1 == E ]:
+if [ $1 == E ]
+then
 	ls /etc > log.$today
-elif [$1 == L ]:
-	ls /etc > log.$today
-elif [$1 == B ]:
-	ls /bin > log.$today
-elif [$1 == H ]:
-	ls /home > log.$today
+elif [$1 == L ]
+then	ls /etc > log.$today
+elif [$1 == B ]
+then	ls /bin > log.$today
+elif [$1 == H ]
+then	ls /home > log.$today
 else 
 	echo "directory doesn't exist!"
 fi
